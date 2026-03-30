af385a1531b1b8c62902bac4078fb1320e87fa27
aasha14 <aaaaaash01@gmail.com>
2026-03-30 11:00:25 +0530

    UC2 Completed

--- App/src/BookMyStayApp.java
+++
@@ -1,9 +0,0 @@
-public class BookMyStayApp {
-    static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay!  modified by 678788");
-        System.out.println("system initialized successfully.   678788");
-        System.out.println("Please select an option:");
-        System.out.println("1. Search for hotels");
-
-    }
-}
