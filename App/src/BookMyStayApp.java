8786e60d9b0f795c732b4a378297f59741113dcd
aasha14 <aaaaaash01@gmail.com>
2026-03-30 10:22:48 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+    static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay!  modified by 678788");
+        System.out.println("system initialized successfully.   678788");
+        System.out.println("Please select an option:");
+        System.out.println("1. Search for hotels");
+
+    }
+}
